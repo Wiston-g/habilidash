668486f5e5fa146abd364d064617b8ae8a7ac204
wiston gonzalez <gonzalesjair54@gmail.com>
2024-01-19 01:23:04 -0500

    Version 1.0.10 habilities ok

--- src/hability/hability.controller.ts
+++ src/hability/hability.controller.ts
@@ -3,45 +3,104 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  UseGuards,
+  Put,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { HabilityService } from './hability.service';
 import { CreateHabilityDto } from './dto/create-hability.dto';
 import { UpdateHabilityDto } from './dto/update-hability.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@ApiBearerAuth()
 @ApiTags('hability')
 @Controller('hability')
 export class HabilityController {
   constructor(private readonly habilityService: HabilityService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createHabilityDto: CreateHabilityDto) {
-    return this.habilityService.create(createHabilityDto);
+  create(@Res() res, @Body() createHabilityDto: CreateHabilityDto) {
+    try {
+      const hability = this.habilityService.create(createHabilityDto);
+      return res.status(HttpStatus.CREATED).json({
+        message: 'User Successfully Created',
+        hability: hability,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error,
+      });
+    }
   }
 
   @Get()
-  findAll() {
-    return this.habilityService.findAll();
+  findAll(@Res() res) {
+    try {
+      const habilities = this.habilityService.findAll();
+      return res.status(HttpStatus.FOUND).json({
+        message: 'habilities found',
+        hability: habilities,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error,
+      });
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.habilityService.findOne(id);
+  findOne(@Res() res, @Param('id') id: string) {
+    try {
+      const hability = this.habilityService.findOne(id);
+      return res.status(HttpStatus.FOUND).json({
+        message: 'hability found',
+        hability: hability,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error,
+      });
+    }
   }
 
-  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
   update(
     @Param('id') id: string,
     @Body() updateHabilityDto: UpdateHabilityDto,
+    @Res() res,
   ) {
-    return this.habilityService.update(id, updateHabilityDto);
+    try {
+      const hability = this.habilityService.update(id, updateHabilityDto);
+      return res.status(HttpStatus.OK).json({
+        message: 'hability Successfully Updated',
+        hability: hability,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error,
+      });
+    }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.habilityService.remove(id);
+  remove(@Res() res, @Param('id') id: string) {
+    try {
+      const hability = this.habilityService.remove(id);
+      return res.status(HttpStatus.ACCEPTED).json({
+        message: 'Hability Successfully Delet',
+        hability: hability,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error,
+      });
+    }
   }
 }
